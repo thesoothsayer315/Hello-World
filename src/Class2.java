c66dbbf590fb8052a670878abcfd36fcff34275b
The Soothsayer <thesoothsayer315@gmail.com>
2016-08-21 01:50:09 -0400

    Added new class to understand class interactions

---
+++ src/Class2.java
@@ -0,0 +1,11 @@
+
+public class Class2 {
+	
+	void method1(){
+		System.out.println("In method 1");
+	}
+	
+	void method1(int number){
+		System.out.println("In method number " +number);
+}
+}
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,6 +5,9 @@ public class HelloWorld {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!!");
 		System.out.println("Adding Changes to the file");
+		Class2 obj = new Class2();
+		//obj.method1();
+		obj.method1(2);
 	}
 
 }
